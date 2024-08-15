9aee103d9e9be09c06b2de7998dce8df14bd7789
josiah <josiah@josiah.microcoin.local>
2024-08-15 12:36:03 +1000

    run_server.sh now runs the server without having to prepare tmux session

--- run_server.sh
+++ run_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SESSION="projects:"
+SESSION="projects"
 WINDOW="mdb_test_auto_server"
 
 # Attempt to kill the window silently
-tmux kill-window -t "$SESSION$WINDOW" 2>/dev/null
-
-# Start a new window in the session
-tmux new-window -n "$WINDOW" -t "$SESSION" "cd mdb-test-auto; poetry install; poetry run mdb_tester; $SHELL"
+tmux kill-window -t "$SESSION:$WINDOW" 2>/dev/null
+tmux new -d -s "$SESSION"
+tmux neww -n "$WINDOW" -t "$SESSION:" "cd mdb-test-auto; poetry install; poetry run mdb_tester; $SHELL"
+tmux switch-client -t "$SESSION:$WINDOW"
